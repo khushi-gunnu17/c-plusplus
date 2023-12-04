a4529bc344e4266488d07d20a64b95e6e625d934
khushi-gunnu17 <khushiastrogeek@gmail.com>
2023-12-04 21:48:34 +0530

    Adding changes

--- 04_Arrays/ninjas_leet_array/rotate_array_189.cpp
+++ 04_Arrays/ninjas_leet_array/rotate_array_189.cpp
@@ -5,27 +5,17 @@ using namespace std;
 void rotateArray(int arr[], int n, int k) {
     int temp[n];
 
-    for(int i=0 ; i<n ; i++) {          // shifting in a cyclic way.
+    // shifting in a cyclic way.
+    for(int i=0 ; i<n ; i++) {          
         temp[(i+k) % n] = arr[i];
     }
 
-    // arr = temp; = not working;
     for(int i=0; i<n; i++) {
         arr[i] = temp[i];
     }
 
     return;
 }
-
-// void rotate(vector<int>& nums, int k) {
-//         vector <int> temp[nums.size()];
-
-//     for(int i=0 ; i<nums.size() ; i++) {
-//         temp[(i+k)%nums.size()] = nums[i];
-//     }
-
-//     nums = temp;
-// }
 
 void printArray(int arr[], int n) {
     for(int i = 0; i<n; i++) {
@@ -47,3 +37,13 @@ int main () {
 
     return 0;
 }
+
+// void rotate(vector<int>& nums, int k) {
+//         vector <int> temp[nums.size()];
+
+//     for(int i=0 ; i<nums.size() ; i++) {
+//         temp[(i+k)%nums.size()] = nums[i];
+//     }
+
+//     nums = temp;
+// }
