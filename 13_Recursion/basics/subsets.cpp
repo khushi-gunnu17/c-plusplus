199fa33f219d502703735036b0c05186c7c4b858
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-02-17 07:07:32 +0530

    Problems added

--- 13_Recursion/basics/subsets.cpp
+++ 13_Recursion/basics/subsets.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 using namespace std;
 
-// pass by reference for ans 
-void solve(vector<int> nums, vector<int> output, int index, vector <vector <int>>& ans) {
-    // Base case 
+// pass by reference for ans
+void solve(vector<int> nums, vector<int> output, int index, vector<vector <int>> &ans) {
+    // base case
     if(index >= nums.size()) {
         ans.push_back(output);
         return;
@@ -13,32 +13,38 @@ void solve(vector<int> nums, vector<int> output, int index, vector <vector <int>
     // exclude 
     solve(nums, output, index + 1, ans);
 
-    // include 
+    // include
     int element = nums[index];
     output.push_back(element);
     solve(nums, output, index + 1, ans);
 }
 
-vector <vector <int>> subsets (vector<int>& nums) {
-    vector <vector<int>> ans;
+vector<vector <int>> subsets(vector<int> &nums) {
+    vector<vector <int>> ans;
     vector <int> output;
     int index = 0;
+
     solve(nums, output, index, ans);
     return ans;
 }
 
 int main() {
-    vector<int> arr(5);
-    arr.push_back(1);
-    arr.push_back(2);
-    arr.push_back(3);
-    arr.push_back(4);
-    arr.push_back(5);
 
-    subsets(arr);
+    vector<int> arr = {1, 2, 3, 4, 5};
 
-    for(int i:arr) {
-        cout << arr[i] << endl;   
+    vector<vector <int>> result = subsets(arr);
+
+    // printing the subsets
+    for (const auto &subset : result) {
+        cout << "[";
+        for(int i = 0; i < subset.size() ; i++) {
+            cout << subset[i];
+            if(i < subset.size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
     }
+
     return 0;
 }
